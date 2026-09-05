a16621e3eefedbafe72965ecd06be88fbf655035
45kikurage-rgb <45kikurage@gmail.com>
2026-09-06 08:02:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='the-fool-quest-v20260906-verification-gap-title3';
+const CACHE='the-fool-quest-v20260906-verification-bottom-gap4';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./title-logo.png','./icon-any.png','./icon-maskable.png'];
 
 self.addEventListener('install',event=>{
