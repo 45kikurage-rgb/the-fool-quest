7e7c13f00a21ed54502b9f56e42cfa25ba043777
45kikurage-rgb <45kikurage@gmail.com>
2026-09-09 20:47:12 +0900

    Fix mobile overflow for Corporate Logo Rounded

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='the-fool-quest-v20260909-corporate-rounded';
+const CACHE='the-fool-quest-v20260909-mobile-fit1';
 const SHARE_CACHE='the-fool-quest-share-v1';
 const ASSETS=['./','./index.html','./style.css','./app.js','./fonts/Corporate-Logo-Rounded-Bold-ver3.woff2','./manifest.webmanifest','./title-logo.png','./icon-any.png','./icon-maskable.png'];
 
