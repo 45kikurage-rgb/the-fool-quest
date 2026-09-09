4eb2dbffb99e87e4e1c7fe68e1fe905113be6abb
45kikurage-rgb <45kikurage@gmail.com>
2026-09-09 21:19:29 +0900

    Tighten portal link panel spacing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='the-fool-quest-v20260909-spacing1';
+const CACHE='the-fool-quest-v20260909-spacing2';
 const SHARE_CACHE='the-fool-quest-share-v1';
 const ASSETS=['./','./index.html','./style.css','./app.js','./fonts/Corporate-Logo-Rounded-Bold-ver3.woff2','./manifest.webmanifest','./title-logo.png','./icon-any.png','./icon-maskable.png'];
 
